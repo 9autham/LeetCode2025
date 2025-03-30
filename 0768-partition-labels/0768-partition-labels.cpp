13536cc9cbb4722553f719b11fb061db2937a905
KURUVA GAUTHAM KUMAR <91959504+9autham@users.noreply.github.com>
2025-03-30 08:05:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.22%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) 
+{
+    unordered_map<char, int> lastIndex;
+    for (int i = 0; i < s.length(); i++) {
+        lastIndex[s[i]] = i;
+    }
+
+    vector<int> partitions;
+    int start = 0, end = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        end = max(end, lastIndex[s[i]]);
+        if (i == end) {
+            partitions.push_back(end - start + 1);
+            start = i + 1;
+        }
+    }
+
+    return partitions;
+}
+};
