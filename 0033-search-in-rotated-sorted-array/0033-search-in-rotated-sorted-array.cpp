e5469ab559d689ac266434035c82c4a9f0b51ed8
KURUVA GAUTHAM KUMAR <91959504+9autham@users.noreply.github.com>
2025-09-27 12:47:32 +0530

    Time: 7 ms (0.47%), Space: 15.1 MB (65.88%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0;
+        int e=nums.size()-1;
+        while(s<=e){
+            cout<<s<<" "<<e<<endl;
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>=nums[0]){
+                if(target>=nums[0]){
+                    if(target>nums[mid]){
+                        s=mid+1;
+                    }
+                    else{
+                        e=mid-1;
+                    }
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(target>=nums[0]){
+                    e=mid-1;
+                }
+                else{
+                    if(target>nums[mid]){
+                        s=mid+1;
+                    }
+                    else{
+                        e=mid-1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
